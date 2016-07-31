5ba06998f55e4cfcf9a925b24dae767fb875aed5
Benjamin Hsu <benhsu75@gmail.com>
2016-07-31 16:30:36 -0700

    Added delete_users endpoint and now should send message to users

--- graph/urls.py
+++ graph/urls.py
@@ -5,4 +5,5 @@ from main import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^messenger_callback/', views.messenger_callback),
+    url(r'^delete_users/', views.delete_users),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from main.models import TestModel
+from main.models import TestModel, User, Goal, GoalEntry
 
 import random
 from django.views.decorators.csrf import csrf_exempt
 import json
+import requests
 
 
 def test(request):
@@ -72,5 +73,44 @@ def messenger_callback(request):
 
 # Sends the user our initial message 
 def handle_authentication(fbid):
-    x = 1
+    # Check if user exists, if it does, do nothing
+    try:
+        existing_user = User.objects.get(fbid=fbid)
+
+        return # Do nothing
+    except User.DoesNotExist:
+        continue
+
+    # If user doesn't exist, create user
+    u = User(fbid=fbid, state=0)
+    u.save()
+
+    # Send user intro message
+    intro_message = "Hey! Nice to meet you. I'm Jarvis, here to help be your better self."
+    send_basic_text_message(fbid, intro_message)
+
+# Helper method to send message
+PAGE_ACCESS_TOKEN = 'EAADqZAUs43F4BAM24X91sSlhAIU7UHnLyO6eNp1rGMmQncyKsz34AgvlqfJKRnn3rNfYLMZBZA914L5z9MO8G6AVsGhljVUZCZAYtNfjbt0NKX7FFHDjOPvBcsiZCNzpSdNVZC4lCsbHVevRIhzKxFzjFzAMDVWq4W8KNuqtxXt8QZDZD'
+SEND_BASE_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token='
+
+def send_basic_text_message(fbid, text):
+    send_payload = {
+        'recipient': {
+            'id': fbid
+        },
+        'message': {
+            'text': text
+        }
+    }
+    url_to_post = SEND_BASE_URL + PAGE_ACCESS_TOKEN
+    r = requests.post(url_to_post, data = send_payload)
+
+def delete_users(request):
+    all_users = User.objects.all()
+    num_of_users_deleted = len(all_users)
+    for u in all_users:
+        u.remove()
+
+    return HttpResponse("Deleted " + num_of_users_deleted + " users!")
+
 
