a943bb9ad21db0040437f4efdf4477b64bb0f59b
Cathy Chen <cathychenn@gmail.com>
2016-08-05 00:58:37 -0700

    add log models and migration file for model creation

--- main/models/log.py
+++ main/models/log.py
@@ -2,35 +2,45 @@ from django.db import models
 from user import *
 
 
+# for now every user only has one log
 class Log(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     user = models.ForeignKey(User)
-    name = models.CharField(max_length=200)
 
+    name = models.CharField(max_length=200)
     # 0: Text
     # 1: Picture
-    # 2: Money
+    # 2: Numeric
     log_type = models.SmallIntegerField()
 
 
-class TextLogEntry(models.Model):
+class LogContext(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    sent_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     log = models.ForeignKey(Log)
 
-    numeric_value = models.FloatField(null=True)
-    binary_value = models.SmallIntegerField(null=True)
-    text_value = models.CharField(max_length=1000, null=True)
-    response_collected = models.SmallIntegerField(default=0)
+    context_name = models.CharField(max_length=200)
 
 
-class PictureLogEntry(models.Model):
-    pass
+class TextLogEntry(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# 
-class MoneyLogEntry:
-    pass
+    log = models.ForeignKey(Log)
+    log_context = models.ForeignKey(LogContext, blank=True, null=True)
+
+    text_value = models.CharField(max_length=1000)
+
+
+class NumericLogEntry(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    log = models.ForeignKey(Log)
+    # doesn't make sense for numeric log to not have a context
+    log_context = models.ForeignKey(LogContext, blank=False, null=False)
+
+    text_value = models.FloatField(default=0)
