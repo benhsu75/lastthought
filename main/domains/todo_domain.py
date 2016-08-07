fc4afa950940d104623a0cf42b61bb0acc9393d5
Benjamin Hsu <benhsu75@gmail.com>
2016-08-06 23:04:18 -0700

    Fixed processed_text bug in handle_todo() function

--- main/domains/todo_domain.py
+++ main/domains/todo_domain.py
@@ -5,11 +5,11 @@ from main.models import *
 
 BASE_HEROKU_URL = 'http://userdatagraph.herokuapp.com'
 
-def handle_todo(current_user, text):
+def handle_todo(current_user, text, processed_text):
 
     fbid = current_user.fbid
     
-    if('add todo' in text):
+    if('add todo' in processed_text):
         # Log response
         message_log.log_message('add_todo_response', current_user, text, None)
 
@@ -25,7 +25,7 @@ def handle_todo(current_user, text):
         message_log.log_message('add_todo_message', current_user, add_todo_message, None)
 
 
-    elif('todo' in text):
+    elif('todo' in processed_text):
         # Log response
         message_log.log_message('show_todo_response', current_user, text, None)
 
