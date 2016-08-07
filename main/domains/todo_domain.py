0766ec49041ca2e0a086095e03f4a3b8e293eb7d
Benjamin Hsu <benhsu75@gmail.com>
2016-08-06 23:14:53 -0700

    Implemented todo list message

--- main/domains/todo_domain.py
+++ main/domains/todo_domain.py
@@ -29,13 +29,40 @@ def handle_todo(current_user, text, processed_text):
         # Log response
         message_log.log_message('show_todo_response', current_user, text, None)
 
-        show_todo_message = "Your todo list:"
+        # Send link to todo list
+        show_todo_message = ""
         send_api_helper.send_button_message(fbid, show_todo_message, [
                 {
                     'type': 'web_url',
                     'url': BASE_HEROKU_URL + '/users/'+fbid+'/todo',
-                    'title': 'To Do List'
+                    'title': 'View your todo list'
                 }
             ])
         message_log.log_message('show_todo_message', current_user, show_todo_message, None)
+
+        # Send todo list inline
+        todo_list_message = generate_todo_list_string(current_user)
+        send_api_helper.send_basic_text_message(fbid, todo_list_message)
+        message_log.log_message('todo_list_message', current_user, todo_list_message, None)
+
+def generate_todo_list_string(current_user):
+    return_string = ''
+
+    todo_list = ToDoTask.objects.filter(user=current_user, completed=False)
+
+    if len(todo_list) == 0:
+        return_string = "You're done with everything!"
+    else:
+        count = 0
+        for todo in todo_list:
+            return_string += str(count) + ') ' + todo.text + '\n'
+            count += 1
+
+    return return_string
+
+
+
+
+
+
 
