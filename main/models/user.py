243f8a251bda58c8837cbe71934f72748614b5c3
Benjamin Hsu <benhsu75@gmail.com>
2016-08-07 00:45:13 -0700

    Added fields for ridesharing on backgroundinformation model

--- main/models/user.py
+++ main/models/user.py
@@ -14,6 +14,12 @@ class BackgroundInformation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # For weather
     current_zip_code = models.CharField(max_length=10, null=True)
+
+    # For ridesharing
+    current_home_address = models.CharField(max_length=200, null=True)
+    current_work_address = models.CharField(max_length=200, null=True)
+    ride_type_preference = models.SmallIntegerField(null=True)
 
     user = models.OneToOneField(User)
