fccf5b8471e53aa3e3f4f73f0ff8793557dbfafc
Benjamin Hsu <benhsu75@gmail.com>
2016-08-07 22:40:03 -0700

    Added user profile helper file

---
+++ main/entrypoints/messenger/fb_profile_helper.py
@@ -0,0 +1,17 @@
+import requests
+from main.utils import constants
+import json
+
+def get_user_profile_data(fbid):
+    url_to_get = constants.GRAPH_BASE_URL + str(fbid) + '?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=' + constants.FB_PAGE_ACCESS_TOKEN
+    r = requests.get(url_to_get)
+    response = json.loads(r.text)
+    return (
+            response['first_name'],
+            response['last_name'],
+            response['profile_pic'],
+            response['locale'],
+            response['timezone'],
+            response['gender'],
+        )
+
--- main/utils/constants.py
+++ main/utils/constants.py
@@ -19,3 +19,5 @@ NEGATIVE_SYNONYMS = [
 FB_PAGE_ACCESS_TOKEN = 'EAADqZAUs43F4BAM24X91sSlhAIU7UHnLyO6eNp1rGMmQncyKsz34AgvlqfJKRnn3rNfYLMZBZA914L5z9MO8G6AVsGhljVUZCZAYtNfjbt0NKX7FFHDjOPvBcsiZCNzpSdNVZC4lCsbHVevRIhzKxFzjFzAMDVWq4W8KNuqtxXt8QZDZD'
 
 FB_SEND_BASE_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token='
+
+GRAPH_BASE_URL = 'https://graph.facebook.com/v2.6/'
