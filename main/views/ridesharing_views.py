f071c1d6536c6dc2c6cb915f58869f80cc382c9e
Benjamin Hsu <benhsu75@gmail.com>
2016-08-08 23:00:17 -0700

    making edits to setup_ridesharing view depending on whether it is set up or not

--- main/views/ridesharing_views.py
+++ main/views/ridesharing_views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from main.models import *
+from main.utils import helper_util
 
+def ridesharing_set_up(fbid):
+    # Get current user
+    if helper_util.user_exists(fbid):
+        current_user = User.objects.get(fbid=fbid)
+    else:
+        return HttpResponse(status=404)
+
+    return current_user.rideshareinformation.connected_flag
 
 def setup(request, fbid):
-    context = RequestContext(request, {
-        'fbid' : fbid
-        })
+
+    # If ridesharing set up, show form to update information
+    if ridesharing_set_up(fbid):
+        context = RequestContext(request, {
+            'ridesharing_set_up_flag' : True,
+            'fbid' : fbid
+            })
+    # Else, allow user to set up ridesharing
+    else:
+        context = RequestContext(request, {
+            'ridesharing_set_up_flag' : False,
+            'fbid' : fbid
+            })
     template = loader.get_template('main/ridesharing_setup.html')
     return HttpResponse(template.render(context))
 
