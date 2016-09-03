be57379f40f8c203fe036fc72d80cdd068f2e1e7
Benjamin Hsu <benhsu75@gmail.com>
2016-09-02 23:44:38 -0400

    Hotfix

--- main/domains/connect_domain.py
+++ main/domains/connect_domain.py
@@ -8,7 +8,7 @@ def foursquare_redirect(request, fbid):
     redirect_uri = 'https://userdatagraph.herokuapp.com/foursquare_redirect'
 
     # Get access token
-    token_url = 'https://foursquare.com/oauth2/access_token?client_id='+constants.FOURSQUARE_CLIENT_ID+'&client_secret='+constants.FOURSQURE_CLIENT_SECRET+'&grant_type=authorization_code&redirect_uri='+redirect_uri+'&code=' + authorization_code
+    token_url = 'https://foursquare.com/oauth2/access_token?client_id='+constants.FOURSQUARE_CLIENT_ID+'&client_secret='+constants.FOURSQUARE_CLIENT_SECRET+'&grant_type=authorization_code&redirect_uri='+redirect_uri+'&code=' + authorization_code
     r = requests.get(token_url)
     if 'access_token' in r.json():
         access_token = r.json()['access_token']
