21638ed738295b55146e7994b74413cc8375d90d
Benjamin Hsu <benhsu75@gmail.com>
2016-09-04 23:36:41 -0400

    Removed additional } when formatting string

--- main/domains/connect_domain.py
+++ main/domains/connect_domain.py
@@ -34,7 +34,7 @@ def uber_redirect(request):
     fbid = request.GET['state']
 
     # Get refresh token
-    token_url = 'https://login.uber.com/oauth/v2/token?client_secret={}&client_id={}&grant_type=authorization_code&redirect_uri={}&code={}}'.format(constants.UBER_CLIENT_ID, constants.UBER_CLIENT_SECRET, 'https://userdatagraph.herokuapp.com/uber_redirect', authorization_code)
+    token_url = 'https://login.uber.com/oauth/v2/token?client_secret={}&client_id={}&grant_type=authorization_code&redirect_uri={}&code={}'.format(constants.UBER_CLIENT_ID, constants.UBER_CLIENT_SECRET, 'https://userdatagraph.herokuapp.com/uber_redirect', authorization_code)
 
     r = requests.get(token_url)
 
