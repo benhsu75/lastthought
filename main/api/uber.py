cdec874b1f790272ebf2b79b5ac264626e633b36
Benjamin Hsu <benhsu75@gmail.com>
2016-09-04 23:41:15 -0400

    asdf

--- main/api/uber.py
+++ main/api/uber.py
@@ -8,4 +8,17 @@ def refresh_ride_history(user):
     x = 1
 
 def refresh_bearer_token(user):
-    
+    # Get refresh token
+    refresh_token = current_user.uberconnection.refresh_token
+
+    # Make request to get access_token
+    payload = {
+        'grant_type' : 'refresh_token',
+        'refresh_token' : refresh_token
+    }
+
+    r = requests.post(OAUTH_URL, data=payload, auth=HTTPBasicAuth(constants.LYFT_CLIENT_ID, constants.LYFT_CLIENT_SECRET));
+
+    bearer_token = r.json()['access_token']
+
+    return bearer_token
