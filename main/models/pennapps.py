a1c59d5f28e6ee1fc99c262b9b8395848456da2f
Benjamin Hsu <benhsu75@gmail.com>
2016-09-10 02:01:34 -0400

    Pennapps

--- main/models/pennapps.py
+++ main/models/pennapps.py
@@ -1,4 +1,8 @@
 from django.db import models
+import datetime
+from django.utils.timesince import timesince
+import pytz
+import math
 
 class InsulinAmount(models.Model):
     amount = models.FloatField()
@@ -6,6 +10,18 @@ class InsulinAmount(models.Model):
 class Dose(models.Model):
     amount = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_time_diff_from_now(self):
+        now = datetime.datetime.now()
+
+        diff_in_min = (now - self.created_at.replace(tzinfo=None)).seconds//60
+
+        if diff_in_min >= 60:
+            to_return = str(int(math.ceil(diff_in_min/60.0))) + ' hours'
+        else:
+            to_return = str(diff_in_min) + ' minutes'
+
+        return to_return
 
 class GlucoseLog(models.Model):
     level = models.FloatField()
