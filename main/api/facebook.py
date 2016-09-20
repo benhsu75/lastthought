e5215b22e2d8d5527f02bb66ed5b7b659f8faa69
Benjamin Hsu <benhsu75@gmail.com>
2016-09-20 02:02:39 -0400

    Fixed mistyping

--- main/api/facebook.py
+++ main/api/facebook.py
@@ -1,9 +1,11 @@
 import requests
 
-FB_API_URL = 'https://graph.api.com/v2.7/'
+FB_API_URL = 'https://graph.facebook.com/v2.7/'
 
 def get_fb_profile_info(access_token):
     user_profile_url = '{}me?access_token={}'.format(FB_API_URL, access_token)
+
+    print 'Going to GET: ' + user_profile_url
 
     r = requests.get(user_profile_url)
 
