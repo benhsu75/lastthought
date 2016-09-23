71addf5808efe5ba72988f996454f3f70b734105
Benjamin Hsu <benhsu75@gmail.com>
2016-09-23 15:54:15 -0400

    Fix

--- main/entrypoints/messenger/fb_profile_helper.py
+++ main/entrypoints/messenger/fb_profile_helper.py
@@ -4,6 +4,7 @@ import json
 
 def get_user_profile_data(fbid):
     url_to_get = constants.GRAPH_BASE_URL + str(fbid) + '?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=' + constants.FB_PAGE_ACCESS_TOKEN
+    print url_to_get
     r = requests.get(url_to_get)
     response = json.loads(r.text)
     print r.text
