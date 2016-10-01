c110fdc0f8e06cace5f0db5cd2902947b759e283
Benjamin Hsu <benhsu75@gmail.com>
2016-10-01 05:04:00 -0400

    Made twilio webhook csrf exempt

--- main/entrypoints/twilio/twilio.py
+++ main/entrypoints/twilio/twilio.py
@@ -1,3 +1,6 @@
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def twilio_callback(request):
     print '=============RECEIVED TWILIO TEXT============'
     print request.POST
